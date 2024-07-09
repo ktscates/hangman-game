95d59351d368c0aedecc9aace2558141f1d6c0e4
ktscates <cates2506@gmail.com>
2024-07-09 17:17:05 +0200

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2463FF",
+        darkNavy: "#261676",
+      },
+
+      backgroundImage: {
+        bgDesktop: "url('../assets/images/background-desktop.svg')",
+        bgMobile: "url('../assets/images/background-mobile.svg')",
+        bgTablet: "url('../assets/images/background-tablet.svg')",
+        pinkGradient: "linear-gradient(to right, #FE71FE, #7199FF)",
+      },
+
+      fontFamily: {
+        primary: ["Mouse Memoirs", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
